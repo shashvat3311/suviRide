572e9387a003499a609c960b8284c0073007a7da
Shashvat <shashvat@rsl.com>
2023-01-30 16:21:41 +0530

    Basic Setup for SERVER/ROUTER/CONTROLLER.js Files

---
+++ server.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const express=require('express')
+const app=express();
+const port=process.env.PORT||5500
+const cors=require('cors')
+const indexrouter=require('./router')
+app.use(express.json())
+
+
+app.use(indexrouter)
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+app.use(cors);
+
+app.listen(port,()=>{
+    console.log(`Server up and Listening on port ${port}`)
+})
